8c32c1b570ff9f140d925d22d43eccff57078a45
yunji1014 <7787yunji@gmail.com>
2026-06-03 00:28:39 +0900

    feat/setting

---
+++ src/main/java/com/codit/boda_prototype/BodaPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.codit.boda_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodaPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodaPrototypeApplication.class, args);
+	}
+
+}
